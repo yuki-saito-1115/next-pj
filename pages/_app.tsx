a93c35a3c1237864d665ff43bc7d5c2b78cb10ab
MLC 齋藤佑樹 <saito-yuuki@mitsue.co.jp>
2022-03-23 02:57:56 +0900

    レストア

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
-import 'node_modules/normalize.css'
-import 'styles/globals.scss'
-import 'styles/utility/index.scss'
+import '@root/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: {
-    Component: any,
-    pageProps: any,
-}) {
-    return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
