e40ea028c372fc5cde066fa6da94017b79d71b18
MLC 齋藤佑樹 <saito-yuuki@mitsue.co.jp>
2022-03-25 15:08:33 +0900

    レストア

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,4 @@
-const { merge } = require('webpack-merge');
-
-const prefix = process.env.STORYBOOK_PREFIX ? `/${process.env.STORYBOOK_PREFIX}` : '';
-
-const getBaseDir = () => {
-  const tsconfig = require('../tsconfig.json');
-  return path.resolve(process.cwd(), tsconfig.compilerOptions.baseUrl);
-};
-
 module.exports = {
-  "staticDirs": [
-    '../public'
-  ],
   'typescript' : {
     reactDocgen: false
   },
@@ -25,32 +13,5 @@ module.exports = {
   'framework': '@storybook/react',
   'core': {
     'builder': 'webpack5'
-  },
-
-  managerHead: (head) => {
-    return `
-      ${head}
-      <link rel="shortcut icon" type="image/x-icon" href="${prefix}/favicon.ico">
-      <script>
-        window['PREVIEW_URL'] = '${prefix}/iframe.html';
-      </script>
-    `;
-  },
-  webpackFinal: async (config) => {
-    return merge(config, {
-      resolve: {
-        modules: [getBaseDir()],
-      },
-      output: {
-        publicPath: `${prefix}/`,
-      },
-    });
-  },
-  managerWebpack: async (config) => {
-    return merge(config, {
-      output: {
-        publicPath: `${prefix}/`,
-      },
-    });
-  },
+  }
 }
--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: '/storybook',
-        destination: '/storybook/index.html',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
